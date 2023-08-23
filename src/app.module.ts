e742e9ffa20de36f49732e2ac1d10365f4631395
Fernando Nicolás <fersanti2896@gmail.com>
2023-08-23 17:55:20 -0600

    Generando resource de Pokedex e implementando Global Prefix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PokedexModule } from './pokedex/pokedex.module';
 
 @Module({
   imports: [ 
     ServeStaticModule.forRoot({
       rootPath: join( __dirname, '..', 'public' ),
-    })
+    }), 
+    PokedexModule
   ]
 })
 export class AppModule {}
